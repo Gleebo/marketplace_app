ff6e1cc47cdb5addb4f79522413d32db9f51d7e0
Gleb <gleb.tsoy754@gmail.com>
2021-03-11 12:33:52 +1100

    add webhook logic

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,5 +3,9 @@ class PaymentsController < ApplicationController
   end
 
   def webhook
+    payment_id = params[:data][:object][:payment_intent]
+    payment = Stripe::PaymentIntent.retrieve(payment_id)
+    event_id = payment.metadata.listing_id
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'payments/success'
-  get 'payments/webhook'
   root "listings#index"
-  get '/search', to: 'listings#search', as: 'search'
   devise_for :users
   resources :listings
   get "/user/listings", to: "listings#user_listings", as: "user_listings"
+  get "/search", to: "listings#search", as: "search"
   get "/payments/success", to: "payments#success"
+  get "/payments/webhook", to: "payments#webhook"
 end
