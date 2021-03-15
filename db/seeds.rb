1dd0d3cd97d012e95b7db5aa0811ab09edd12b6a
Gleb <gleb.tsoy754@gmail.com>
2021-03-15 13:06:24 +1100

    add categories seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+["GPU", "CPU", "peripherals", "monitors", "motherboards", "PSU", "RAM", "SSD", "HDD"].each do |cat|
+  Category.create(name: cat)
+end
